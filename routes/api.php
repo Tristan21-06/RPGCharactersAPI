417a671a08038dacb11438adf756607a136ec9ed
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2025-05-21 11:23:09 +0200

    feat: add characters api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Controller\CharacterController;
 use App\Controller\TypeController;
+
+$router->get('/personnages', CharacterController::class .'@list');
+$router->get('/personnage/{name}', CharacterController::class .'@show');
+$router->post('/personnage', CharacterController::class .'@new');
+$router->patch('/personnage/{name}', CharacterController::class .'@update');
+$router->delete('/personnage/{name}', CharacterController::class .'@delete');
 
 $router->get('/types', TypeController::class .'@list');
 $router->get('/type/{id}', TypeController::class .'@show');
