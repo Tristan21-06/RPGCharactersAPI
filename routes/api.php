5c9b713e4d0c3cce5fa6ac9ede9b0b5887f47845
BDMaxiTacos <HideSnake.Gaming@gmail.com>
2025-05-21 14:01:39 +0200

    feat: change routes for create and update

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,12 @@ use App\Controller\TypeController;
 
 $router->get('/personnages', CharacterController::class .'@list');
 $router->get('/personnage/{name}', CharacterController::class .'@show');
-$router->post('/personnage', CharacterController::class .'@new');
-$router->patch('/personnage/{name}', CharacterController::class .'@update');
+$router->post('/personnage/create', CharacterController::class .'@new');
+$router->patch('/personnage/update/{name}', CharacterController::class .'@update');
 $router->delete('/personnage/{name}', CharacterController::class .'@delete');
 
 $router->get('/types', TypeController::class .'@list');
 $router->get('/type/{id}', TypeController::class .'@show');
-$router->post('/type', TypeController::class .'@new');
-$router->patch('/type/{id}', TypeController::class .'@update');
+$router->post('/type/create', TypeController::class .'@new');
+$router->patch('/type/update/{id}', TypeController::class .'@update');
 $router->delete('/type/{id}', TypeController::class .'@delete');
